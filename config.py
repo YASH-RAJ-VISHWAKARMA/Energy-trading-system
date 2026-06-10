6ae6b55c4bb921525eed99cb9c847ccd6404c082
YASH RAJ VISHWAKARMA <17.yash.raj.v@gmail.com>
2026-06-11 04:37:02 +0530

    Adding more to config

--- config.py
+++ config.py
@@ -6,10 +6,7 @@ load_dotenv()
 CITY = "Bhopal"
 DATABASE_NAME = "energy_trading.db"
 
-OPENWEATHER_API_KEY = os.getenv(
-    "OPENWEATHER_API_KEY"
-)
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+EIA_API_KEY = os.getenv("EIA_API_KEY")
 
-EIA_API_KEY = os.getenv(
-    "EIA_API_KEY"
-)
+# print(f"Loaded API Keys: OPENWEATHER_API_KEY={OPENWEATHER_API_KEY}, EIA_API_KEY={EIA_API_KEY}")
